d6d688a4f9e25e690004d7baf14952267858c326
Mohamed Boudra <boudra.moha@gmail.com>
2015-09-05 23:37:08 +0200

    delete custom i3status

--- i3/i3status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-i3status -c ~/.i3/i3status.conf | while :
-do
-    read line
-    song="`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$` "-" `dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`"
-    echo "$line |  $song"
-done
