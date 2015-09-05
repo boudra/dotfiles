73733a9947674fdd3b56447c661497fb142f829e
Mohamed Boudra <boudra.moha@gmail.com>
2015-09-06 00:34:31 +0200

    safer and better install script

--- install.sh
+++ install.sh
@@ -1,15 +1,57 @@
 #!/bin/bash
-dir=~/dotfiles
 
-exclude=("install.sh", ".git", ".gitignore")
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )
 
-cd $dir
+FILES=$( find ${DIR}/* -maxdepth 0 \
+    -not -name 'install.sh' \
+    -not -name '.git' \
+    -not -name '.gitignore' \
+    -not -name '.gitmodules' \
+    -not -path .
+)
 
-for file in  *; do
-    for ((i = 0; i < ${#exclude[@]}; i++)); do
-        if [[ ${file} != ${exclude[${index}]} ]]; then
-            rm -rf ~/.${file}
-            ln -s ${dir}/${file} ~/.${file}
+GLOBAL_OVERWRITE=0
+
+while getopts "f" o; do
+    case "${o}" in
+        f)
+            GLOBAL_OVERWRITE=1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+for FILE in $FILES; do
+
+    NAME=$(basename $FILE)
+    DEST=${HOME}/.${NAME}
+    SRC=${DIR}/${NAME}
+    OVERWRITE=$GLOBAL_OVERWRITE
+
+    if [ -e $DEST ]; then
+
+        if [ $OVERWRITE -eq 0 ]; then
+            read -r -p "\`.$NAME' already exists. Do you want to overwrite it?  [y/N] " RESPONSE
+            if ! [[ $RESPONSE =~ ^([yY][eE][sS]|[yY])$  ]]; then
+                continue
+            fi
         fi
-    done
+
+    fi
+
+    if [ $NAME == "gitconfig" ]; then
+        read -r -p "Please specify the author email address to use for git: " EMAIL
+        read -r -p "Please specify the author name to use for git (usually your full name): " NAME
+        rm -rf ${DEST}
+        cp ${SRC} ${DEST}
+        sed -i "s/{{\s*EMAIL\s*}}/$EMAIL/g" ${DEST}
+        sed -i "s/{{\s*NAME\s*}}/$NAME/g" ${DEST}
+        continue
+    fi
+
+    rm -rf ${DEST}
+    ln -s ${SRC} ${DEST}
+
+    echo "$DEST installed."
+
 done
