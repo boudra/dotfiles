043204f6a726e8aa72938d84663206df06b6f88e
Mohamed Boudra <boudra.moha@gmail.com>
2021-05-21 10:48:34 +0200

    fix typescript

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,7 +8,7 @@ fish_vi_key_bindings
 eval (direnv hook fish)
 
 set -g fisher_path ~/.config/fish/fisher
-set -x FZF_DEFAULT_COMMAND "ag -g ''"
+set -x FZF_DEFAULT_COMMAND "ag -g '' --hidden --ignore .git"
 set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 
 set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
@@ -23,3 +23,4 @@ function t
 end
 
 source /usr/local/opt/asdf/asdf.fish
+set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
