f4bebd0443751b3969670832ba4b47d3d8249e7f
Mohamed Boudra <boudra.moha@gmail.com>
2025-12-21 23:12:59 +0700

    feat: add agl agent loop script and update configs
    Add autonomous agent loop tool for running tasks from plan files.
    Remove render-markdown.nvim plugin and disable always-thinking mode.
    Comment out local claude alias.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -110,14 +110,6 @@ return require('packer').startup(function(use)
           },
         },
       },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
     },
   }
 
